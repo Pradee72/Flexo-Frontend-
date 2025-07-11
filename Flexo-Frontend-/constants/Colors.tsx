39cd54ad6421764635ce7d83f15fb862a7841c56
ShervinDani <rvshervin@gmil.com>
2025-07-11 21:07:19 +0530

    Added

---
+++ Flexo-Frontend-/constants/Colors.tsx
@@ -0,0 +1,7 @@
+const Colors = {
+    primary: '#ffff',
+    secondary: '#A259FF',
+    // secondary: '#1C1F4A',
+    text: '#2C2C2C',
+};
+export default Colors;
---
+++ Flexo-Frontend-/screens/LoginScreen.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export const LoginScreen = () => {
+    return (<Text>Hello</Text>)
+};
